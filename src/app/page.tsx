ebd58bd8e52dc473aaee25e99e26875a72950c75
Julia Bower <julia@thefractionaldirectory.com>
2026-01-07 10:29:47 +1300

    replace root page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,64 +3,53 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li>
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="budgeting.html">Budgeting</a>
+          </li>
+          <li>
+            <a href="academic.html">Academic Help</a>
+          </li>
+          <li>
+            <a href="adjusting.html">Adjusting to Uni Life</a>
+          </li>
+          <li>
+            <a href="figuring.html">Figuring out your career path</a>
+          </li>
+          <li>
+            <a href="which-uni.html">Which Uni should I go to?</a>
+          </li>
+          <li>
+            <a href="about.html">About</a>
+          </li>
+          <li>
+            <a href="student-loan.html">Applying for a student Loan</a>
+          </li>
+        </ul>
+      </nav>
+      <header>
+        <h1>UniHelp NZ</h1>
+        <p>A simple guide to help students navigate university</p>
+      </header>
+      <main>
+        <p>
+          Starting university can feel overwhelming. Suddenly you’re in a new
+          environment, surrounded by unfamiliar systems, endless resources, and
+          expectations to figure everything out quickly. From lectures and
+          tutorials to study tools, degrees, majors, and career paths... it’s a
+          lot to process, especially when you’re still unsure about what you
+          want. UniHelp NZ was created to give students a calm starting point. A
+          place to explore university life, reflect on what suits you, and
+          understand your options without pressure. This isn’t about having all
+          the answers, instead, it’s about helping you take your next step with
+          a bit more clarity and confidence.
+        </p>
       </main>
-    </div>
+    </>
   );
 }
